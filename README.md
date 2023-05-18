d8dce66c44182e1183fcb01ce8b4fdef9e2a6a29
Mariam22-hub <mariam.elmoazen@gmail.com>
2023-05-19 01:27:39 +0300

    game basics

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 # Ai-agent
+=======
+# Connect4-Python
+Connect 4 programmed in python using pygame.
+
+Video walkthrough on programming this game: https://youtu.be/UYgyRArKDEs
+
+Video walkthrough on programming the AI: https://youtu.be/MMLtza3CZFM
+>>>>>>> 8e73b8c (game basics)
---
+++ connect4.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pygame
+import sys
+import math
+
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+def create_board():
+	board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+	return board
+
+def drop_piece(board, row, col, piece):
+	board[row][col] = piece
+
+def is_valid_location(board, col):
+	return board[ROW_COUNT-1][col] == 0
+
+def get_next_open_row(board, col):
+	for r in range(ROW_COUNT):
+		if board[r][col] == 0:
+			return r
+
+def print_board(board):
+	print(np.flip(board, 0))
+
+
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0
